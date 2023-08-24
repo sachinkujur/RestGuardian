265019c3878d608daf1c248fc6889b634f67576e
Sachin <sachinkujurcs13@gmail.com>
2023-08-24 17:44:17 +0530

    Create BaseTest class

--- src/test/java/GetBoardsTest.java
+++ src/test/java/GetBoardsTest.java
@@ -1,30 +1,11 @@
-import io.restassured.RestAssured;
 import io.restassured.module.jsv.JsonSchemaValidator;
-import io.restassured.specification.RequestSpecification;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-
-public class GetBoardsTest {
-
-    private static RequestSpecification requestWithAuth() {
-        return RestAssured.given()
-                .queryParams(Map.of(
-                        "key", "73f7a097a4567b388231c8ea06e7866d",
-                        "token", "ATTA188495fbbe30a3764bb69bd7965c1be94e2af93f400f94aad463c50e5dff418705084EF1"
-                ));
-    }
-
-    @BeforeAll
-    public static void setBaseUrl(){
-        RestAssured.baseURI = "https://api.trello.com";
-    }
+public class GetBoardsTest extends BaseTest {
 
     @Test
-    public void checkGetBoards(){
+    public void checkGetBoards() {
         requestWithAuth()
                 .queryParam("fields", "id,name")
                 .pathParam("member", "sachinkujur")
@@ -35,7 +16,7 @@ public class GetBoardsTest {
     }
 
     @Test
-    public void checkGetBoard(){
+    public void checkGetBoard() {
         requestWithAuth()
                 .queryParam("fields", "id,name")
                 .pathParam("id", "64799a8e4853e983424c00d3")
--- src/test/java/GetBoardsValidationTest.java
+++ src/test/java/GetBoardsValidationTest.java
@@ -1,26 +1,10 @@
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-public class GetBoardsValidationTest {
-
-    private static RequestSpecification requestWithAuth() {
-        return RestAssured.given()
-                .queryParams(Map.of(
-                        "key", "73f7a097a4567b388231c8ea06e7866d",
-                        "token", "ATTA188495fbbe30a3764bb69bd7965c1be94e2af93f400f94aad463c50e5dff418705084EF1"
-                ));
-    }
-
-    @BeforeAll
-    public static void setBaseUrl() {
-        RestAssured.baseURI = "https://api.trello.com";
-    }
+public class GetBoardsValidationTest extends BaseTest {
 
     @Test
     public void checkGetBoardWithInvalidId() {
@@ -35,7 +19,7 @@ public class GetBoardsValidationTest {
 
     @Test
     public void checkGetBoardWithInvalidAuth() {
-        Response response = RestAssured.given()
+        Response response = requestWithoutAuth()
                 .pathParam("id", "64799a8e4853e983424c00d3")
                 .get("/1/boards/{id}");
         response
@@ -46,7 +30,7 @@ public class GetBoardsValidationTest {
 
     @Test
     public void checkGetBoardWithAnotherUserCredentials() {
-        Response response = RestAssured.given()
+        Response response = requestWithoutAuth()
                 .queryParams(Map.of(
                         "key", "73f7a097a4567b388231c8ea06e7866d",
                         "token", "018459018349936a264bdcb07b41fa4b538b501ef504fee5d07ea9f86b953e9\n"
