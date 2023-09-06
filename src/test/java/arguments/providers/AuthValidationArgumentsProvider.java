5bcd2aa165c048a77deef10133882c7e8cd267ab
Sachin <sachinkujurcs13@gmail.com>
2023-09-06 15:48:46 +0530

    DELETE Method Validation with Rest Assured

--- src/test/java/arguments/providers/AuthValidationArgumentsProvider.java
+++ src/test/java/arguments/providers/AuthValidationArgumentsProvider.java
@@ -21,7 +21,7 @@ public class AuthValidationArgumentsProvider implements ArgumentsProvider {
                 ),
                 new AuthValidationArgumentsHolder(
                         Map.of("key", UrlParamValues.VALID_KEY),
-                        "unauthorized card permission requested"
+                        "unauthorized permission requested"
                 ),
                 new AuthValidationArgumentsHolder(
                         Map.of("token", UrlParamValues.VALID_TOKEN),
